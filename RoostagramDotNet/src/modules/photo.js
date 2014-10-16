1525981208078dff3219afc7ce4bc73da811a6b6
GoodGuyJim <jnardecchia@outlook.com>
2014-10-16 16:53:55 -0400

    Add photo model
    This is roughly equivalent to the commit in the original Roostagram that
    can be found here:
    
    https://github.com/bocoup-education/roost-chicago-2014-app/tree/3d2dad3d6c95caba62a8e667a458098d84e820ff

---
+++ RoostagramDotNet/src/modules/photo.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    'use strict';
+
+    var Photo = Backbone.Model.extend({
+        urlRoot: function () {
+            return 'http://localhost:8001/photos';
+        }
+    });
+
+    window.Photo = Photo;
+}());
