ce6c06008d5fb0c3d8d9516319a6e78d0d3a9123
GoodGuyJim <jnardecchia@outlook.com>
2014-10-16 17:33:42 -0400

    Add single photo view
    This is roughly equivalent to the commit in the original Roostagram that
    can be found here:
    
    https://github.com/bocoup-education/roost-chicago-2014-app/tree/45e64a30db29446d6be6ffe9fce3c814e882330f

--- RoostagramDotNet/src/modules/photo.js
+++ RoostagramDotNet/src/modules/photo.js
@@ -2,9 +2,7 @@
     'use strict';
 
     var Photo = Backbone.Model.extend({
-        urlRoot: function () {
-            return 'http://localhost:8001/photos';
-        }
+        urlRoot: 'http://localhost:8001/photos'
     });
 
     window.Photo = Photo;
---
+++ RoostagramDotNet/src/modules/view.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+
+    var PhotoView = Backbone.View.extend({
+        render: function () {
+
+            // add to the DOM
+            $('<img>', {
+                src: this.model.get('dataUri')
+            }).appendTo(this.$el);
+
+            return this;
+        }
+    });
+
+    window.PhotoView = PhotoView;
+}());
