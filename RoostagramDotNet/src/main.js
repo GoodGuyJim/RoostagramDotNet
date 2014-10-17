cc94b596342a045577c5480e926787e1d6be7eda
GoodGuyJim <jnardecchia@outlook.com>
2014-10-17 14:04:11 -0400

    Create a router and a test route
    This is roughly equivalent to the commit in the original Roostagram that
    can be found here:
    
    https://github.com/bocoup-education/roost-chicago-2014-app/tree/9e1665a038c7d9fe27a9743330d4623bee4cf09f

--- RoostagramDotNet/src/main.js
+++ RoostagramDotNet/src/main.js
@@ -1,12 +1,17 @@
 define(function(require) {
     'use strict';
 
-    // photo collection view
+    var Backbone = require('backbone');
     var PhotoCollection = require('src/modules/components/photo/collection');
     var GalleryView = require('src/modules/components/photo/gallery');
 
+    require('modules/core/router');
+
+    Backbone.history.start();
+
     var photos = new PhotoCollection();
 
+    // Create gallery view with supplied photo collection
     var gallery = new GalleryView({
         collection: photos,
         el: '#app'
---
+++ RoostagramDotNet/src/modules/core/router.js
@@ -0,0 +1,18 @@
+﻿define(function (require) {
+    'use strict';
+    var Backbone = require('backbone');
+
+    var Router = Backbone.Router.extend({
+
+        routes: {
+            '': 'hello'
+        },
+
+        hello: function () {
+            alert('hello Roost!');
+        }
+    });
+
+    return new Router();
+
+});
