5244fb7968c14a68c8e08415d65ab069ab7e6c3b
GoodGuyJim <jnardecchia@outlook.com>
2014-10-16 23:48:02 -0400

    Build and reorganize modules folder into component-based hierarchy
    This is roughly equivalent to the commit in the original Roostagram that
    can be found here:
    
    https://github.com/bocoup-education/roost-chicago-2014-app/tree/8b12f3f2caddcec173b185f57b5c2498bf3030e0

--- RoostagramDotNet/src/main.js
+++ RoostagramDotNet/src/main.js
@@ -1,7 +1,7 @@
 define(function(require) {
     'use strict';
-    var Photo = require('src/modules/photo');
-    var PhotoView = require('src/modules/view');
+    var Photo = require('src/modules/components/photo/model');
+    var PhotoView = require('src/modules/components/photo/view');
 
     var photo = new Photo({
         id: 19
