ccdccf6799ae493426c8e714ea2f58624ae024b6
GoodGuyJim <jnardecchia@outlook.com>
2014-10-17 10:48:14 -0400

    Create photo collection constructor
    This is roughly equivalent to the commit in the original Roostagram that
    can be found here:
    
    https://github.com/bocoup-education/roost-chicago-2014-app/tree/71a141e05f2ebcb8a42926341656a1e452f2ccf5

---
+++ RoostagramDotNet/src/modules/components/photo/collection.js
@@ -0,0 +1,16 @@
+﻿define(function (require) {
+    'use strict';
+
+    var Backbone = require('backbone');
+    var Photo = require('src/modules/components/photo/model');
+
+    var PhotoCollection = Backbone.Collection.extend({
+        model: Photo,
+        comparator: function (model) {
+            return (1 / model.id);
+        },
+        url: 'http://localhost:8001/photos'
+    });
+
+    return PhotoCollection;
+});
